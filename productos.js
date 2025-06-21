b21da5728b8f8fa4c64aeb1dc59b847f0cc02738
martinrodass <juanmrodas27@gmail.com>
2025-06-21 18:18:13 -0300

    separacion de disponibles

--- productos.js
+++ productos.js
@@ -25,7 +25,7 @@ function verificarDisponibilidad(prod){
 
 
 
-function printProductos(container,pCategoria, todo){
+function printProductos(container,pCategoria, disp, todo){
 //trae el archivio
 fetch('productos.json')
 //entonces convierte la respuesta en objetos
@@ -46,7 +46,10 @@ fetch('productos.json')
       let cuotasHTML =verificarCuotas(producto);
       let disponibilidad = verificarDisponibilidad(producto);
       
-      //bloque de cracion de divs solo si tiene descuento
+    
+
+    if(producto.stock >= 1){
+        //bloque de cracion de divs solo si tiene descuento
       if (producto.descuento !== 0) {
           div.innerHTML = `
           <div class="container-img-product">
@@ -82,11 +85,84 @@ fetch('productos.json')
       //agregar div al contenedor
       contenedor.appendChild(div);
        }
+      }
+      
     });
   })
   .catch(error => console.error("Error cargando productos:", error));
 }
 
-printProductos("descartables","descartable");
+function printProductosNoDisp(container,pCategoria, disp, todo){
+//trae el archivio
+fetch('productos.json')
+//entonces convierte la respuesta en objetos
+  .then(res => res.json())
+//entonces se accede a los objetos como objeto llamado productos
+  .then(productos => {
+    const contenedor = document.getElementById(container);
 
-printProductos("recargables","recargable");
+    //para cada producto en productos va a hacer esto
+    productos.forEach(producto => {
+      //seleccion por categoria
+
+    if(producto.categoria == pCategoria || todo){
+          const div = document.createElement("div");
+      div.className = "producto";
+
+      // verifica si el producto seleccionado en el momento se puede en cuotas y disponibilidad
+      let cuotasHTML =verificarCuotas(producto);
+      let disponibilidad = verificarDisponibilidad(producto);
+      
+    
+
+    if(producto.stock < 1){
+        //bloque de cracion de divs solo si tiene descuento
+      if (producto.descuento !== 0) {
+          div.innerHTML = `
+          <div class="container-img-product">
+            <img src="${producto.img}" alt="Imagen de ${producto.nombre}">
+          </div>
+          <div class="container-info-product">
+            <h4>${producto.nombre}</h4>
+            <p class="descuento">$<del>${producto.precio.toLocaleString("es-AR")}</del> <span>${producto.descuento * 100}%OFF</span> </p>
+            <p class="precio">$${(producto.precio * producto.descuento).toLocaleString("es-AR")}</p>
+            <p class="descripcion">${producto.descripcion.replace(/\n/g, "<br>")}</p>
+            ${disponibilidad}
+            ${cuotasHTML}
+            <a class="comprar" href="https://wa.me/5493794860589?text=Hola!%20Estoy%20interesado%20en%20${encodeURIComponent(producto.nombre)}" target="_blank">consultar</a>
+          </div>
+        `;
+      } 
+      // bloque solo si no tiene descuento
+      else { 
+        div.innerHTML = `
+          <div class="container-img-product">
+            <img src="${producto.img}" alt="Imagen de ${producto.nombre}">
+          </div>
+          <div class="container-info-product">
+            <h4>${producto.nombre}</h4>
+            <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
+            <p class="descripcion">${producto.descripcion.replace(/\n/g, "<br>")}</p>
+            ${disponibilidad}
+            ${cuotasHTML}
+            <a class="comprar" href="https://wa.me/5493794860589?text=Hola!%20Estoy%20interesado%20en%20${encodeURIComponent(producto.nombre)}" target="_blank">consultar</a>
+          </div>
+        `;
+      }
+      //agregar div al contenedor
+      contenedor.appendChild(div);
+       }
+      }
+      
+    });
+  })
+  .catch(error => console.error("Error cargando productos:", error));
+}
+
+
+
+printProductos("descartables","descartable","disponible");
+
+printProductos("recargables","recargable","disponible");
+
+printProductosNoDisp("pedido","pedido","no", true);
